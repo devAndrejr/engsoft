58cb738a910097b73eedb402ab67a055bbea5f70
devAndrejr <79088224+devAndrejr@users.noreply.github.com>
2021-02-15 08:31:27 -0300

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,19 @@
+print('Graça e Paz!')
+print()
+
+print('Qual é o seu nome?') #pergunta o nome
+Nome = input()
+
+print('Prazer em te conhecer,'+ Nome) # Saudação
+
+print('O comprimento do seu nome tem:')
+print(len(Nome),'letras') # Quantidade de caracteres
+
+print('Qual é a sua idade?') #pergunta a idade
+idade = input()
+
+print('Sua idade tem:')
+print(len(idade),' números')
+
+
+print('Você terá ' + str(int(idade)+ 1)+' anos de idade em um ano.')
